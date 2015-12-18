69e3bc41a1e3e004a1fae5c9f504f5a801ad832b
Timothy Follo <tbf5@chameleon.zoo.cs.yale.edu>
2015-12-18 15:23:27 -0500

    oxford mostly cleaned

--- Unstrucuted Dictionaries/CleanDicts.py
+++ Unstrucuted Dictionaries/CleanDicts.py
@@ -29,31 +29,43 @@ parenEx = re.compile("\(\W*(?:-ting|usu\.|prefix|n\.|v\.|pl\.|adj\.|abbr\.|contr
 #  [ABCDEFGHJKLMNOPQRSTUVWXYZ&] 
 letterEx = re.compile(" [ABCDEFGHJKLMNOPQRSTUVWXYZ&] ")
 
-with open (outfile, "w") as outfile:
-	if mode == "oxford":
-		for line in open(infile, 'rb'):
-			line = line.strip()						# Isolate line
-			if line == "":							# Ignore empty lines
-				continue
-			else:
-				word = wordEx.search(line)
-				if word and "  " not in word.group():
-					definition = re.sub(wordEx, "", line, 1)#defEx.findall(line)
-					#for defn in definitions:
-					outfile.write("{}\n".format(word.group()))
+explainEx = re.compile("(?:.*$|= \*.*$|\([^\)]*\))")
 
-					splits = re.split(splitEx, definition)
-					for defn in splits:
-						if defn and defn != "" and defn != " ":
-							splits2 = re.split(parenEx, defn)
-							for defn2 in splits2:
-								if defn2 and defn2 != "" and defn2 != " ":
-									splits3 = re.split(letterEx, defn2)
-									for defn3 in splits3:
-										if defn3 and defn3 != "" and defn3 != " ":
-											outfile.write(":\t{}\n".format(defn3.strip()))
+with open (outfile, "w") as outfile:
+	with open ('clean.log', 'w') as logfile:
+		if mode == "oxford":
+			for line in open(infile, 'rb'):
+				line = line.strip()						# Isolate line
+				if line == "":							# Ignore empty lines
+					continue
 				else:
-					print "{}".format(line)
+					word = wordEx.search(line)
+					if word and "  " not in word.group():
+						definition = re.sub(wordEx, "", line, 1)#defEx.findall(line)
+						#for defn in definitions:
+						logfile.write("{}\n".format(word.group()))
+
+						splits = re.split(splitEx, definition)
+						for defn in splits:
+							if defn and defn != "" and defn != " ":
+								splits2 = re.split(parenEx, defn)
+								for defn2 in splits2:
+									if defn2 and defn2 != "" and defn2 != " ":
+										splits3 = re.split(letterEx, defn2)
+										for defn3 in splits3:
+											if defn3 and defn3 != "" and defn3 != " " and defn3 != "&":
+												splits4 = re.split(explainEx, defn3)
+
+												# print '\n'
+												# print defn3
+												# print splits4
+
+												for defn4 in splits4:
+													if defn4 and len(defn4) > 3:
+														logfile.write(":\t{}\n".format(defn4.strip()))
+														outfile.write("{}\t{}\n".format(word.group(), defn4.strip()))
+					else:
+						logfile.write("\nBAD WORD: {}".format(line))
 
 # filepath = sys.argv[1]
 # print "Printing to: {}".format(filepath)
