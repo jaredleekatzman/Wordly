9db2d9cc28cdbaeebaec54c5db4877cc8938a30d
Jared Katzman <jaredleekatzman@gmail.com>
2015-12-22 01:44:27 -0500

    Look_up for Wordly.py

--- Word_Model/Model/Wordly.py
+++ Word_Model/Model/Wordly.py
@@ -1,5 +1,7 @@
 from keras.models import model_from_json
-
+from process_data import sen_to_idx, padd_sentence, get_sentence_embeddings
+from operator import itemgetter
+from itertools import izip, islice
 
 class Singleton(object):
   _instance = None
@@ -77,87 +79,14 @@ class Wordly(Singleton):
                 print(i, '\t', word, '\t\t', distance)
         return (neighbors,distance)
 
-    def sen_to_idx(sentence, word2id):
-        def case_normalizer(word, dictionary):
-            """ In case the word is not available in the vocabulary,
-             we can try multiple case normalizing procedure.
-             We consider the best substitute to be the one with the lowest index,
-             which is equivalent to the most frequent alternative."""
-            w = word
-            lower = (dictionary.get(w.lower(), 1e12), w.lower())
-            upper = (dictionary.get(w.upper(), 1e12), w.upper())
-            title = (dictionary.get(w.title(), 1e12), w.title())
-            results = [lower, upper, title]
-            results.sort()
-            index, w = results[0]
-            if index != 1e12:
-                return w
-            return word
-        def normalize(word, word_id):
-            """ Find the closest alternative in case the word is OOV.
-            Returns the index of the word
-            """
-            # Noramlize digits by replacing them with #
-            DIGITS = re.compile("[0-9]", re.UNICODE)
-            
-            if not word in word_id:
-                word = DIGITS.sub("#", word)
-            if not word in word_id:
-                word = case_normalizer(word, word_id)
+    def look_up(query, k = 5, verbose = False):
+        idx = sen_to_idx(query, self.word_id, self.maxlen)
+        matrix = numpy.asarray(list(idx))
+        definition = get_sentence_embeddings(matrix, self.embeddings)
 
-            if not word in word_id:
-                return None
-            return word
+        pred = self.model.predict(definition, verbose = int(verbose))
 
-        """
-        Function that takes a dictionary entry and word and maps 
-        it to the given vocab list word2id
-        """
-        if isinstance(sentence,list):
-            return map(lambda s: sen_to_idx(s, word2id), sentence)
-        
-        # Remove punctuation and split sentence into words
-        sentence = re.sub(r'[^\w\s]','',sentence) 
-        words = sentence.split(' ')
-        
-        # Normalize Each Word
-        words = map(lambda w: normalize(w, word2id), words)
-        
-        # Assumes 0 is the <UNK> token
-        idx = [word2id.get(word, 0) for word in words]
-        return idx
-
-    def padd_sentence(sent, maxlen, tokens, include_sens_tags = False):
-        """
-        Takens an individual sentence SENT
-        Padds it with PADD to the specified MAXLEN
-        """
-        tags = 2 * int(include_sens_tags)
-        if len(sent) > (maxlen - tags):
-            sent = sent[:(maxlen - tags)]
-
-        if include_sens_tags:
-            sent = [tokens['<S>']] + sent + [tokens['</S>']]
-
-        sent = sent + [tokens['<PAD>']] * (maxlen - len(sent))
-        return sent
-
-    def get_sentence_embeddings(sent, embeddings):
-        """
-        Takes a matrix of sent (NUM_SENT, MAXLEN)
-        and embeddings (NUM_WORDS, EMBED_SIZE)
-        Map each word of each sentence to its emebeddings 
-        """
-        num_sent, maxlen = sent.shape
-        num_words, embed_size = embeddings.shape
-        sen_embed = np.zeros((num_sent, maxlen, embed_size))
-        for i in range(num_sent):
-            for j in range(maxlen):
-                sen_embed[i,j,:] = embeddings[sent[i,j]]
-
-    return sen_embed
-
-    def look_up(query, k = 5):
-
+        neighbors, distance = knn(pred, k, verbose = verbose)
+        return izip(neighbors, distance)
 
         
